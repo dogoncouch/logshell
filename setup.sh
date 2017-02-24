9f227daf08d642774d6745868219da7bab5aabc6
dogoncouch <dpersonsdev@gmail.com>
2017-02-24 03:31:47 -0500

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# MIT License
+# 
+# Copyright (c) 2017 Dan Persons <dpersonsdev@gmail.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+if [ -w /usr/bin ]; then
+    echo Write privileges in /usr/bin, installing there.
+    cp logshell.sh /usr/bin/logshell
+    chmod 755 /usr/bin/logshell
+else
+    echo No write privileges in /usr/bin, installing in ~/bin.
+    mkdir -p ~/bin
+    cp logshell.sh ~/bin/logshell
+    echo Add ~/bin to PATH in ~/.profile.
+fi
